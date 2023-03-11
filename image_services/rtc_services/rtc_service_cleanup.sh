7880025f5d86aa027e3c0395ceb6c7de6933b9bc
jacquelynsmale <34557291+jacquelynsmale@users.noreply.github.com>
2023-03-10 15:43:44 -0900

    add git pull

--- image_services/rtc_services/rtc_service_cleanup.sh
+++ image_services/rtc_services/rtc_service_cleanup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#pull any remote changes
+
+git pull origin main
+
 #Script that sorts through rtc image_services directories and deletes geodatabases and service definitions > a day old
 
 find /home/arcgis/gis-services/image_services/rtc_services/ -name *gdb -type d -mmin +10800 -exec rm -Rv {} \;
