f4470fb8ea93c7fe9f6f797c035628d10914ee87
jacquelynsmale <34557291+jacquelynsmale@users.noreply.github.com>
2023-03-10 16:16:48 -0900

    add in hypen not underscore

--- image_services/rtc_services/rtc_service_cleanup.sh
+++ image_services/rtc_services/rtc_service_cleanup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #pull any remote changes
-cd /home/arcgis/gis_services
+cd /home/arcgis/gis-services
 git pull
 
 #Script that sorts through rtc image_services directories and deletes geodatabases and service definitions > a day old
