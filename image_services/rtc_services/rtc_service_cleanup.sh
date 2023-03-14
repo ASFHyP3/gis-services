1085cc8814a2f1fcf193ec67b97411ce28441a74
jacquelynsmale <34557291+jacquelynsmale@users.noreply.github.com>
2023-03-14 13:48:47 -0800

    chmod for just rtc_service_cleanup

--- image_services/rtc_services/rtc_service_cleanup.sh
+++ image_services/rtc_services/rtc_service_cleanup.sh
@@ -3,7 +3,6 @@
 #pull any remote changes
 cd /home/arcgis/gis-services
 git pull
-find /home/arcgis/gis-services/image_services/rtc_services/ -name *.sh -exec chmod 777 {} \;
 
 #Script that sorts through rtc image_services directories and deletes geodatabases and service definitions > a day old
 
