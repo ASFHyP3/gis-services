0226405b9e73c921cbcca2365a58408a11460317
hjkristenson <hjkristenson@alaska.edu>
2023-07-07 10:34:54 -0800

    populate Enabled and TypeName elements

--- image_services/rtc_services/etree_code.py
+++ image_services/rtc_services/etree_code.py
@@ -3,14 +3,17 @@ from lxml import etree
 service_draft = r'C:\Users\hjkristenson\PycharmProjects\gis-services\image_services\rtc_services\USDA_RTC_VV_230707_0039.sddraft'
 service_draft_out = r'C:\Users\hjkristenson\PycharmProjects\gis-services\image_services\rtc_services\USDA_RTC_VV_test.sddraft'
 
-XHTML_NAMESPACE = 'http://www.w3.org/2001/XMLSchema-instance'
-XHTML = "{%s}" % XHTML_NAMESPACE
-NSMAP = {None : XHTML_NAMESPACE}
+# XHTML_NAMESPACE = 'http://www.w3.org/2001/XMLSchema-instance'
+# XHTML = "{%s}" % XHTML_NAMESPACE
+# NSMAP = {None : XHTML_NAMESPACE}
+
+# NSMAP = {None : 'xsi'}
 
 tree = etree.parse(service_draft)
 sub_el_extensions = tree.find("/Configurations/SVCConfiguration/Definition/Extensions")
-sub_el_svcextension = etree.SubElement(sub_el_extensions, 'SVCExtension', type='typens:SVCExtension',
-                                       nsmap=NSMAP)
+# sub_el_svcextension = etree.SubElement(sub_el_extensions, 'SVCExtension', type='typens:SVCExtension',
+#                                        nsmap=NSMAP)
+sub_el_svcextension = etree.SubElement(sub_el_extensions, 'SVCExtension', type='typens:SVCExtension')
 
 svc_exts = [('Enabled', '', ''), ('Info', 'typens:PropertySet', {'xsi':'http://www.w3.org/2001/XMLSchema-instance'}),
             ('Props', 'typens:PropertySet', {'xsi':'http://www.w3.org/2001/XMLSchema-instance'}), ('TypeName', '', '')]
@@ -23,6 +26,8 @@ for sub_el in svc_exts:
         etree.SubElement(sub_el_path, 'PropertyArray', type='typens:ArrayOfPropertySetProperty')
         print(f'nsmap={sub_el[2]}')
 
+tree.find("/Configurations/SVCConfiguration/Definition/Extensions/SVCExtension/Enabled").text = 'true'
+
 sub_props = tree.find("/Configurations/SVCConfiguration/Definition/Extensions/SVCExtension/Info/PropertyArray")
 for el in [('WebEnabled', 'true'),
            ('WebCapabilities', 'GetCapabilities,GetMap,GetFeatureInfo,GetStyles,GetLegendGraphic,GetSchemaExtension')]:
@@ -33,11 +38,13 @@ for el in [('WebEnabled', 'true'),
     value.text = el[1]
 
 sub_props = tree.find("/Configurations/SVCConfiguration/Definition/Extensions/SVCExtension/Props/PropertyArray")
-for i in range(21):
+for el in range(21):
     add_prop = etree.SubElement(sub_props, 'PropertySetProperty', type='typens:PropertySetProperty')
     etree.SubElement(sub_props, 'PropertySetProperty', type='typens:PropertySetProperty')
     etree.SubElement(add_prop, 'Key')
     etree.SubElement(add_prop, 'Value', type='xs:string')
+
+tree.find("/Configurations/SVCConfiguration/Definition/Extensions/SVCExtension/TypeName").text = 'WMSServer'
 
 # It might be easier to just use the Element Maker to construct this whole section, then insert it into the sddraft
 # after it's been amended as currently done. But maybe it wouldn't be easier.
