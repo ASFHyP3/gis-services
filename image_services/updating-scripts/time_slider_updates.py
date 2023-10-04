42614c242b4e840bef62fd4158d69e20d4dd51ce
jrsmale <jrsmale@alaska.edu>
2023-10-04 15:48:59 -0800

    add in the timeStopInterval dictionary

--- image_services/updating-scripts/time_slider_updates.py
+++ image_services/updating-scripts/time_slider_updates.py
@@ -9,17 +9,19 @@ def datetime_to_esri(date_time):
     return calendar.timegm(date_time.timetuple()) * 1000
 
 
-webmap_id = '0b0336feb86e47b1a2b6ddf40b0d5bf3'
+webmap_id = 'bcd14ca972cf455eae744661744927df'
+
 gis = GIS('home')
 item = gis.content.get(webmap_id)
 
 data = item.get_data()
-
 slider_length = datetime.timedelta(weeks=51)
 window_length = datetime.timedelta(weeks=3)
+buffer_length = datetime.timedelta(days=1)
 latest_time = data['widgets']['timeSlider']['properties']['endTime']
-end_time = datetime.date.fromtimestamp(latest_time/1000)
-
+end_time = datetime.date.fromtimestamp(latest_time/1000) + buffer_length
+time_stop_interval = {'interval': 1, 'units': 'esriTimeUnitsDays'}
+data['widgets']['timeSlider']['properties']['timeStopInterval'] = time_stop_interval
 
 data['widgets']['timeSlider']['properties']['startTime'] = datetime_to_esri(end_time - slider_length)
 data['widgets']['timeSlider']['properties']['currentTimeExtent'] = [datetime_to_esri(end_time - window_length),
