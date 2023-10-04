9d0c32776bfb7c38578cd4d3e354cc88616a5e46
jrsmale <jrsmale@alaska.edu>
2023-10-04 15:10:31 -0800

    Move the window to the last 3 weeks

--- image_services/updating-scripts/time_slider_updates.py
+++ image_services/updating-scripts/time_slider_updates.py
@@ -4,14 +4,25 @@ import json
 
 from arcgis import GIS
 
+
+def datetime_to_esri(date_time):
+    return calendar.timegm(date_time.timetuple()) * 1000
+
+
 webmap_id = '0b0336feb86e47b1a2b6ddf40b0d5bf3'
 gis = GIS('home')
 item = gis.content.get(webmap_id)
 
 data = item.get_data()
 
-delta_t = datetime.timedelta(weeks=3)
-end_time = datetime.date.fromtimestamp(data['widgets']['timeSlider']['properties']['endTime']/1000)
-data['widgets']['timeSlider']['properties']['startTime'] = calendar.timegm((end_time - delta_t).timetuple()) * 1000
+slider_length = datetime.timedelta(weeks=51)
+window_length = datetime.timedelta(weeks=3)
+latest_time = data['widgets']['timeSlider']['properties']['endTime']
+end_time = datetime.date.fromtimestamp(latest_time/1000)
+
+
+data['widgets']['timeSlider']['properties']['startTime'] = datetime_to_esri(end_time - slider_length)
+data['widgets']['timeSlider']['properties']['currentTimeExtent'] = [datetime_to_esri(end_time - window_length),
+                                                                    datetime_to_esri(end_time)]
 
 item.update(data=json.dumps(data))
