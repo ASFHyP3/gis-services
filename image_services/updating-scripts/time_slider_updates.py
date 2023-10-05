261417d978b532b8b593df6caaa733def9c4e9a4
jrsmale <jrsmale@alaska.edu>
2023-10-05 11:18:25 -0800

    Add in all ASF webmaps we'd like to update

--- image_services/updating-scripts/time_slider_updates.py
+++ image_services/updating-scripts/time_slider_updates.py
@@ -9,22 +9,36 @@ def datetime_to_esri(date_time):
     return calendar.timegm(date_time.timetuple()) * 1000
 
 
-webmap_id = 'a3452a1f0df54e62a8ec84fb36fda344'
+def update_time_slider(item):
+    data = item.get_data()
+    slider_length = datetime.timedelta(weeks=51)
+    window_length = datetime.timedelta(weeks=3)
+    buffer_length = datetime.timedelta(days=1)
 
-gis = GIS('home')
-item = gis.content.get(webmap_id)
+    today_time = datetime.date.today() + buffer_length
+    data['widgets']['timeSlider']['properties']['endTime'] = datetime_to_esri(today_time)
 
-data = item.get_data()
-slider_length = datetime.timedelta(weeks=51)
-window_length = datetime.timedelta(weeks=3)
-buffer_length = datetime.timedelta(days=1)
-latest_time = data['widgets']['timeSlider']['properties']['endTime']
-end_time = datetime.date.fromtimestamp(latest_time/1000) + buffer_length
-time_stop_interval = {'interval': 1, 'units': 'esriTimeUnitsDays'}
-data['widgets']['timeSlider']['properties']['timeStopInterval'] = time_stop_interval
+    time_stop_interval = {'interval': 1, 'units': 'esriTimeUnitsDays'}
+    data['widgets']['timeSlider']['properties']['timeStopInterval'] = time_stop_interval
 
-data['widgets']['timeSlider']['properties']['startTime'] = datetime_to_esri(end_time - slider_length)
-data['widgets']['timeSlider']['properties']['currentTimeExtent'] = [datetime_to_esri(end_time - window_length),
-                                                                    datetime_to_esri(end_time)]
+    data['widgets']['timeSlider']['properties']['startTime'] = datetime_to_esri(today_time - slider_length)
+    data['widgets']['timeSlider']['properties']['currentTimeExtent'] = [datetime_to_esri(today_time - window_length),
+                                                                        datetime_to_esri(today_time)]
 
-item.update(data=json.dumps(data))
+    return item.update(data=json.dumps(data))
+
+
+webmap_ids = ['2205f66af0324a88a8d0b8a6c8fde5bf',  # Alaska Rivers
+              '80442ecd1e0246adac5f5fb7e627e3e3',  # HKH
+              '3dd8d25559db4ba6aa0e1b6e8cb5d39a',  # RTC
+              'faa83e4ccfe64bb8a99c13ef70b19b8f',  # SWE
+              ]
+
+gis = GIS('pro')
+
+for webmap_id in webmap_ids:
+    item = gis.content.get(webmap_id)
+    if update_time_slider(item):
+        print(f'Updated webmap with id {webmap_id}')
+    else:
+        print(f'Error updating {webmap_id}')
