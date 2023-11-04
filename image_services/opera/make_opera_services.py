037c2931e2ba56b81b6fa4db221b616eb63e49b9
jacquelynsmale <jacquelyn.smale@gmail.com>
2023-11-03 17:26:16 -0800

    add in aws exploration changes

--- image_services/opera/make_opera_services.py
+++ image_services/opera/make_opera_services.py
@@ -186,7 +186,7 @@ def main():
 
     raster_store = '/home/arcgis/raster_store/'
     bucket = 'hyp3-testing'
-    overview_path = '/vsis3/hyp3-nasa-disasters/overviews/'
+    overview_path = '/vsis3/gis-service-overviews/overviews/'
 
     template_directory = Path(__file__).parent.absolute() / 'raster_function_templates'
 
