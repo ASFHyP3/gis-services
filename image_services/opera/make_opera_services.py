488561ed6f945cc278c63617df8f9957d6899de4
hjkristenson <hjkristenson@alaska.edu>
2023-11-06 16:11:28 -0900

    add new dictwriter

--- image_services/opera/make_opera_services.py
+++ image_services/opera/make_opera_services.py
@@ -96,6 +96,7 @@ def update_csv(csv_file: str, rasters: List[str]):
 
         for raster in new_rasters:
             record = get_raster_metadata(raster)
+            writer = csv.DictWriter(csvfile, fieldnames=record.keys(), lineterminator=os.linesep)
             logging.info(f'Adding {raster} to {csv_file}')
             writer.writerows(record)
 
