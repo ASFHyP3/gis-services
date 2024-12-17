dfd027149b6d6b27033ca80a357298c989b0027c
Andrew Player <andrewplayer3@gmail.com>
2024-12-17 12:49:21 -0500

    ruff config

--- image_services/updating_scripts/time_slider_updates.py
+++ image_services/updating_scripts/time_slider_updates.py
@@ -5,9 +5,10 @@ import json
 import boto3
 from arcgis import GIS
 
-client = boto3.client(service_name="secretsmanager", region_name="us-west-2")
-response = client.get_secret_value(SecretId="tools_user_accounts")
-password_dict = json.loads(response["SecretString"])
+
+client = boto3.client(service_name='secretsmanager', region_name='us-west-2')
+response = client.get_secret_value(SecretId='tools_user_accounts')
+password_dict = json.loads(response['SecretString'])
 
 
 def datetime_to_esri(date_time):
@@ -21,12 +22,12 @@ def update_time_slider(item):
     buffer_length = datetime.timedelta(days=1)
 
     today_time = datetime.date.today() + buffer_length
-    time_slider = data["widgets"]["timeSlider"]["properties"]
+    time_slider = data['widgets']['timeSlider']['properties']
 
-    time_slider["endTime"] = datetime_to_esri(today_time)
-    time_slider["timeStopInterval"] = {"interval": 1, "units": "esriTimeUnitsDays"}
-    time_slider["startTime"] = datetime_to_esri(today_time - slider_length)
-    time_slider["currentTimeExtent"] = [
+    time_slider['endTime'] = datetime_to_esri(today_time)
+    time_slider['timeStopInterval'] = {'interval': 1, 'units': 'esriTimeUnitsDays'}
+    time_slider['startTime'] = datetime_to_esri(today_time - slider_length)
+    time_slider['currentTimeExtent'] = [
         datetime_to_esri(today_time - window_length),
         datetime_to_esri(today_time),
     ]
@@ -35,20 +36,20 @@ def update_time_slider(item):
 
 
 webmap_ids = [
-    "2205f66af0324a88a8d0b8a6c8fde5bf",  # Alaska Rivers
-    "80442ecd1e0246adac5f5fb7e627e3e3",  # HKH
-    "3dd8d25559db4ba6aa0e1b6e8cb5d39a",  # RTC
-    "faa83e4ccfe64bb8a99c13ef70b19b8f",  # SWE
+    '2205f66af0324a88a8d0b8a6c8fde5bf',  # Alaska Rivers
+    '80442ecd1e0246adac5f5fb7e627e3e3',  # HKH
+    '3dd8d25559db4ba6aa0e1b6e8cb5d39a',  # RTC
+    'faa83e4ccfe64bb8a99c13ef70b19b8f',  # SWE
 ]
 
 gis = GIS(
-    "https://asf-daac.maps.arcgis.com",
-    password_dict["asf-agol-username"],
-    password_dict["asf-agol-password"],
+    'https://asf-daac.maps.arcgis.com',
+    password_dict['asf-agol-username'],
+    password_dict['asf-agol-password'],
 )
 
 for webmap_id in webmap_ids:
     if update_time_slider(gis.content.get(webmap_id)):
-        print(f"Updated webmap with id {webmap_id}")
+        print(f'Updated webmap with id {webmap_id}')
     else:
-        print(f"Error updating {webmap_id}")
+        print(f'Error updating {webmap_id}')
