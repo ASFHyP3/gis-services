70d7ec543a6c158d616021ce588961d611b283d2
Andrew Johnston <asjohnston@alaska.edu>
2025-07-29 10:51:59 -0800

    update script

--- image_server/server_setup.sh
+++ image_server/server_setup.sh
@@ -6,12 +6,15 @@ echo ""
 read -sp 'asf_publisher password: ' asf_publisher_password
 echo ""
 
+sudo apt update
+sudo apt upgrade -y
+
 echo "increasing limit on open file descriptors for ubuntu user"
 echo 'ubuntu soft nofile 65535' | sudo tee -a /etc/security/limits.conf
 ulimit -S -n 65535
 
 echo "installing AWS CLI"
-sudo apt install unzip
+sudo apt install zip
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
@@ -35,9 +38,9 @@ echo "installing arcpy conda environment"
 wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh
 bash Miniforge3-Linux-x86_64.sh -b
 source /home/ubuntu/miniforge3/etc/profile.d/conda.sh
+mamba shell init --shell=bash
 mamba env create -f /home/ubuntu/gis-services/image_server/environment.yml -y
 conda env config vars set ARCGISHOME=/home/ubuntu/arcgis/server/ -n arcpy
-mamba shell init
 rm Miniforge3-Linux-x86_64.sh
 
 echo "creating server_connection.json"
